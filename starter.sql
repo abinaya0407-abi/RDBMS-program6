821822339913d30a57e1c13c6080f0b3954072bf
abinaya0407-abi <abinayarajendran0407@gmail.com>
2026-09-02 13:08:17 +0530

    Create Student1 table and perform CRUD operations
    Replaced Student table with Student1 and added operations.

--- starter.sql
+++ starter.sql
@@ -1,19 +1,27 @@
 
 CREATE DATABASE IF NOT EXISTS CollegeDB;
-
 USE CollegeDB;
 
-DROP TABLE IF EXISTS Student;
-
-CREATE TABLE Student (
+DROP TABLE IF EXISTS Student1;
+CREATE TABLE Student1 (
     StudentID INT PRIMARY KEY,
     StudentName VARCHAR(20) NOT NULL,
     Gender VARCHAR(10),
     DepartmentID INT
 );
+desc Student1;
 
-INSERT INTO Student (StudentID, StudentName, Gender, DepartmentID)
+INSERT INTO Student1 (StudentID, StudentName, Gender, DepartmentID)
 VALUES
 (1001, 'Arun', 'Male', 101),
 (1002, 'Divya', 'Female', 102),
 (1003, 'Karthik', 'Male', 101);
+select * from Student1;
+
+UPDATE Student1
+SET DepartmentID = 103
+WHERE StudentID = 1003;
+  
+DELETE from Student1
+WHERE StudentID = 1002;
+select * from Student1;
